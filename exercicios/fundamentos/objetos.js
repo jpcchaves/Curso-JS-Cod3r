abfc6828c7a49c4e76a14abb54134106f32c6907
João Paulo <95300143+jpcchaves@users.noreply.github.com>
2022-03-24 16:52:24 -0300

    aula sobre objetos

--- exercicios/fundamentos/objetos.js
+++ exercicios/fundamentos/objetos.js
@@ -1,5 +1,12 @@
 const prod1 = {};
 prod1.nome = "Celular LEGAL";
 prod1.preco = 998.99;
+prod1['desconto legal'] = 0.40; // evitar atributos com espaço
 
 console.log(prod1);
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preço: '79.90'
+}
+console.log(prod2)
