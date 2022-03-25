4ab0e8021435eafadcbc9f90e060b568fa0b69cb
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-03-24 22:43:08 -0300

    aula sobre typeof Object e Class

---
+++ exercicios/fundamentos/funcaoEmQuaseTudo.js
@@ -0,0 +1,8 @@
+//Object é uma função
+console.log(typeof Object);
+
+//class é uma função
+class Produto {}
+console.log(typeof Produto)
+
+//Obs. Quando você quer instanciar um objeto (gerar um objeto a partir de uma class), na verdade, você está instanciando uma função
