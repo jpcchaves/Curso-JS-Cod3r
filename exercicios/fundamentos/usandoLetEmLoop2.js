b98a53c78db3d1c35ebff1b18d8f9e61f006377a
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-03-26 10:12:54 -0300

    aula sobre repetição com var e let

---
+++ exercicios/fundamentos/usandoLetEmLoop2.js
@@ -0,0 +1,8 @@
+//Estrutura de repetição com Let
+for (let i = 0; i < 10; i++){
+    console.log(`o valor é ${i}`)
+}
+
+console.log(`i = ${i}`) // dá erro porque o I não foi definido fora do bloco
+
+//let tem escopo de bloco
