b1ce2d0f772328f5912fe049d8c010a166aab493
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-03-26 13:05:26 -0300

    operadores aritmeticos

---
+++ exercicios/fundamentos/aritmeticos.js
@@ -0,0 +1,15 @@
+//Operadores Aritméticos
+
+const [a,b,c,d] = [3,5,1,15];
+
+const soma = a+b+c+d; // soma
+const subtracao = d-c; // subtração
+const mult = a*b; // multiplicação
+const div = d / a; // divisão
+const resto = a%2; // resto da divisão
+
+console.log(soma)
+console.log(subtracao)
+console.log(mult)
+console.log(div)
+console.log(resto) // se o resto for igual a 1, ele é ímpar. Se for = 0, é par.
