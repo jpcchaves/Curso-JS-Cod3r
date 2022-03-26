b3a44f6de90d1ad12011a8fba2d8dbeaf271c555
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-03-26 12:13:00 -0300

    destructuring 1

---
+++ exercicios/fundamentos/destructuring1.js
@@ -0,0 +1,24 @@
+// recurso novo no ES2015
+
+const pessoa = {
+    nome: 'João',
+    idade: 24,
+    endereco:{
+        logradouro: 'Melhor rua da cidade',
+        bairro: 'Melhor bairro da cidade',
+        numero: 08
+    }
+}
+
+//Como acessar algum elemento do objeto?
+//Operador de desestruturação { }
+const { nome, idade } = pessoa; // tire de dentro do objeto o atributo nome e idade
+console.log(nome, idade)
+
+//atribuindo a outras variáveis: "n", "i"
+const { nome: n, idade: i} = pessoa;
+console.log(n, i);
+
+//acessando um objeto dentro de um objeto
+const { endereco: {logradouro: rua, numero: numCasa}} = pessoa;
+console.log(rua, numCasa)
