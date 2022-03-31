cee7376b1b111aa31531c58a786bb97087c4a165
João Paulo <95300143+jpcchaves@users.noreply.github.com>
2022-03-31 11:45:06 -0300

    resolução primeiro desafio

---
+++ ListasDeExercicios/Fundamentos, Estruturas de Controle e Funções/minhas resoluções/1aQuestao.js
@@ -0,0 +1,9 @@
+//Resolução da primeira questão
+
+//Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração, multiplicação e divisão desses valores.
+
+operacoes = (n1, n2) => {
+    console.log(`Soma:`, n1 + n2,'// Substração:', n1 - n2,'// Mult:', n1 * n2,'// Divisão:', n1 / n2,'// Divisão inversa:', n2 / n1)
+
+}
+operacoes(10,5)
