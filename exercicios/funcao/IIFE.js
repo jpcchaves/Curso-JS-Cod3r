0a2a4a042cb23825b7fc0688421f73c22ed732ed
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-03-30 22:09:53 -0300

    IEE - funçao automática

---
+++ exercicios/funcao/IIFE.js
@@ -0,0 +1,7 @@
+// IIFE : IMMEDIATELy INVOKED FUNCTION EXPRESSION
+// Expressão de função imediatamente invocada / automaticamente
+
+(function(){
+    console.log('Será executado na hora!!!!')
+    console.log('Fugindo do escopo global')
+})()
