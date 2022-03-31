6983eab30d7e7e972cf3a099a093b3286f0abbb5
João Paulo <95300143+jpcchaves@users.noreply.github.com>
2022-03-31 15:57:23 -0300

    resolução 4ª questão

---
+++ ListasDeExercicios/Fundamentos, Estruturas de Controle e Funções/minhas resoluções/4aQuestao.js
@@ -0,0 +1,6 @@
+// Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado e o resto da divisão destes dois valores.
+
+retornaResto = (n1, n2) => {
+    return console.log((n1 / n2), (n1 % n2))
+}
+retornaResto(10,3)
