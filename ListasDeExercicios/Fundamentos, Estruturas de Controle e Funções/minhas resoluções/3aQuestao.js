2ed5a1eddeb0d3f5f404e3a1b5f4a40df665f468
João Paulo <95300143+jpcchaves@users.noreply.github.com>
2022-03-31 15:27:51 -0300

    solução 3ª questão

---
+++ ListasDeExercicios/Fundamentos, Estruturas de Controle e Funções/minhas resoluções/3aQuestao.js
@@ -0,0 +1,6 @@
+// Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+expoente = (base, expoente) => {
+    return console.log(Math.pow(base, expoente))
+}
+expoente(2,2)
