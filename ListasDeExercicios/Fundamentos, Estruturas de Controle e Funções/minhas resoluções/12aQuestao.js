910bca5d018f7687673ed02d560370f84125f902
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-04-02 11:36:58 -0300

    solução 12ª questão

---
+++ ListasDeExercicios/Fundamentos, Estruturas de Controle e Funções/minhas resoluções/12aQuestao.js
@@ -0,0 +1,20 @@
+// Faça um algoritmo que calcule o fatorial de um número.
+
+fatorial = num => {
+    let fatorial =  num;
+    let resultado = fatorial;
+    for (let i = 1; i < fatorial; i++){
+        resultado *= i; // ou resultado = resultado * i
+    }
+    console.log(resultado)
+}
+fatorial(1)
+fatorial(2)
+fatorial(3)
+fatorial(4)
+fatorial(5)
+fatorial(6)
+fatorial(7)
+fatorial(8)
+fatorial(9)
+fatorial(10)
