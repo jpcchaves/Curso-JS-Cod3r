4d8ef7bf47eae8e58c298ae1698edc4eee94a06b
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-04-02 12:29:40 -0300

    solução 16ª questão

---
+++ ListasDeExercicios/Fundamentos, Estruturas de Controle e Funções/minhas resoluções/16aQuestao.js
@@ -0,0 +1,30 @@
+// Utilizando a estrutura do Switch faça um programa que simule uma calculadora básica.
+// O programa recebe como parâmetros dois valores numéricos e uma string referente à operação e a realize com os valores numéricos na ordem que foram inseridos.
+// Por exemplo: calculadora (2, ‘+’, 3).
+// A função efetuará a soma de 2 e
+// 3. Dica: Os sinais das operações são: ‘+’. ‘-’, ‘*’ e ‘/’. 
+// Crie um caso default para operações inválidas.
+
+calculadora = (num1, num2, operador) => {
+    switch (operador){
+        case "+":
+            return console.log(num1 + num2)
+        case "-":
+            return console.log(num1 - num2)
+        case "*":
+            return console.log(num1 * num2)
+        case "/":
+            return console.log(num1 / num2)
+        default:
+            return console.log(`Operador inválido.`)
+    }
+}
+calculadora(10,20,"+")
+calculadora(10,20,"-")
+calculadora(10,20,"*")
+calculadora(10,20,"/")
+calculadora(10,20,"")
+calculadora(10,20," ")
+calculadora(10,20,"operador  ???")
+calculadora(10,20,"=&&*&*¨*")
+calculadora(10,20,"TESTE")
