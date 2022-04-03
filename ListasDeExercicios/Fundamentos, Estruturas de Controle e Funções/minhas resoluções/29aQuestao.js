9b5361d98d85dc065e4b4a8569f8ea9f0958acff
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-04-03 12:49:39 -0300

    solução 29ª questão

---
+++ ListasDeExercicios/Fundamentos, Estruturas de Controle e Funções/minhas resoluções/29aQuestao.js
@@ -0,0 +1,26 @@
+// Utilizando a estrutura de repetição for, faça uma função que percorra um vetor e conte quantos números deste vetor estão no intervalo [10,20] (repare que o intervalo é fechado, ou seja, inclui o 10 e o 20) e quantos deles estão fora do intervalo, escrevendo estas informações.
+
+const numeros = [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11,
+    12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+    24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+    36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+    48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+    60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
+    72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
+    84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
+    96, 97, 98, 99, 100];
+
+percorreVetor = vetor => {
+    let intervalo = 0;
+    let foraIntervalo = 0;
+    for (let i = 1; i <= vetor.length; i++) {
+        if (vetor[i] >= 10 && vetor[i] <= 20){
+            intervalo++
+        } else {
+            foraIntervalo++
+        }
+    }
+    console.log("Dentro do Intervalo:", intervalo)
+    console.log("Fora do Intervalo:", foraIntervalo)
+}
+percorreVetor(numeros)
