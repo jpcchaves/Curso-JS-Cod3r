54f3087b33771961a82ccd744c10423de51c9b10
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-04-03 15:50:39 -0300

    solução 39ª questão

--- ListasDeExercicios/Fundamentos, Estruturas de Controle e Funções/minhas resoluções/39aQuestao.js
+++ ListasDeExercicios/Fundamentos, Estruturas de Controle e Funções/minhas resoluções/39aQuestao.js
@@ -1,3 +1,6 @@
+// Crie uma função que receba dois vetores de igual tamanho e troque seus elementos de modo que o primeiro elemento do vetorA passe a ser o primeiro elemento do vetorB e vice versa e assim sucessivamente.
+// Faça a troca sem utilizar uma variável auxiliar.
+
 function trocaValores(vetorA, vetorB) {
     if (vetorA.length == vetorB.length) {
         for(let i = 0; i < vetorA.length; i++){
