1930a608ba5b203e4d4beb03bbf542682be852f9
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-04-09 11:05:04 -0300

    método flatmap

---
+++ array/flatMap.js
@@ -0,0 +1,29 @@
+const escola = [{
+    nome: "Turma M1",
+    alunos: [{
+        nome: 'Gustavo',
+        nota: 8.1
+    }, {
+        nome: 'Ana',
+        nota: 9.3
+    }]
+}, {
+    nome: "Turma M2",
+    alunos: [{
+        nome: 'Rebeca',
+        nota: 8.9
+    }, {
+        nome: 'Roberto',
+        nota: 7.3
+    }]
+}]
+
+// Extraindo as notas
+const getNotaAluno = aluno => aluno.nota
+const getNotasDaTurma = turma => turma.alunos.map(getNotaAluno)
+
+const notas1 = escola.map(getNotasDaTurma)
+console.log(notas1)
+
+const notas2 = escola.flatMap(getNotasDaTurma)
+console.log(notas2)
