6f5bfe2befb7aaca9d5bfad323d444cfeeef14b8
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-04-13 22:27:18 -0300

    como inverter array

---
+++ array/invertendoArray.js
@@ -0,0 +1,10 @@
+function segundoMaior(numeros) {
+    // invertendo o array
+  const numerosOrdenados = numeros.sort(
+    (numeroA, numeroB) => numeroB - numeroA
+  );
+  console.log(numerosOrdenados)
+  const segundoMaior = numerosOrdenados[1];
+  return segundoMaior;
+}
+console.log(segundoMaior([1,2,3,4,5,6]))
