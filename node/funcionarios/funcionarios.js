6cc385b1d9e39939aa57c77092fae1c3d215402f
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-04-15 13:05:57 -0300

    filtrando base de dados

---
+++ node/funcionarios/funcionarios.js
@@ -0,0 +1,31 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+// Axios é um client http, ele faz requisições para obter informações de algo que está remoto
+
+// ====== DESAFIO =======
+    // Encontrar a MULHER CHINESA com o menor SALARIO
+
+// Cria uma função que filtra o PAÍS
+    const chineses = f => f.pais === "China" 
+
+// Cria uma função que filtra se é MULHERES
+    const mulheres = f => f.genero === "F"
+
+// Cria uma função reduce que encontra o menor salário
+    const menorSalario = (func, funcAtual) => {
+        return func.salario < funcAtual.salario ? func : funcAtual
+    }
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+    console.log(funcionarios)
+
+    // Mulher chinesa com o menor salário
+    const func = funcionarios
+    .filter(chineses)
+    .filter(mulheres)
+    .reduce(menorSalario)
+
+    console.log(func)   
+})
