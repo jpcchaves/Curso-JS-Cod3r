39f8af1ad4ac30725a536ba789a3aaa13fb1f042
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-04-15 23:54:51 -0300

    passando parâmetros entre modulos

---
+++ node/passandoParametros2.js
@@ -0,0 +1,4 @@
+// Passando parâmetros para o módulo1 (passandoParametros.js)
+
+const saudacoes = require('./passandoParametros')('Ana', 'Zezinha', 'Claudinha', 'Querida', 'Rosa')
+console.log(saudacoes)
