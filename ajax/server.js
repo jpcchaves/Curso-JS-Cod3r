2a8088f3a99e8b1895691cde389bf19e948d2d80
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-05 22:58:23 -0300

    ajax #02

--- ajax/server.js
+++ ajax/server.js
@@ -13,5 +13,31 @@ const app = express()
     // Se for JSON, vai transformá-lo em objeto
     app.use(bodyParser.json())
 
+    // interpreta o formulário que veio o arquivo do upload
+    const multer = require('multer')
+
+    // diskStorage recebe um objeto e esse objeto serve para personalizar a pasta que os arquivos serão salvos e o nome dos arquivos
+    const storage = multer.diskStorage({
+        destination: function(req, file, callback) {
+            callback(null, './upload')
+        },
+        filename: function(req, file, callback){
+            callback(null, `${Date.now}_${file.originalname}`)
+        }
+    })
+
+
+const upload = multer( { storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
+
 app.get('/teste', (req, res) => res.send('OK'))
 app.listen(8080, () => console.log('Executando'))
