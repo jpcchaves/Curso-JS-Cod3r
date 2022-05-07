b3ef53130954234725ca36feb29e76fc1be88a9f
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-07 16:48:17 -0300

    implementado servidor (falta finalizar)

--- gulp/spa/gulpTasks/servidor.js
+++ gulp/spa/gulpTasks/servidor.js
@@ -1,14 +1,23 @@
-const gulp = require('gulp')
+const gulp = require("gulp");
+const webserver = require("gulp-webserver");
+const watch = require("gulp-watch");
 
-function monitorarArquivos(cb){
-    return cb()
+function servidor(cb) {
+  return gulp.src("build").pipe(
+    webserver({
+      port: 8080,
+      open: true,
+      livereload: true,
+    })
+  );
 }
 
-function servidor(cb){
-    return cb()
-}
+function monitorarArquivos(cb) {
+  watch("src/**/*.html", () => gulp.series("appHTML")());
 
+  return cb();
+}
 module.exports = {
-    monitorarArquivos,
-    servidor
-}
+  monitorarArquivos,
+  servidor,
+};
