d15d3f7d5c346efea33364c3ee958fc2750f3b50
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-07 13:34:14 -0300

    SASS p/ CSS com gulp

--- gulp/css/gulpfile.js
+++ gulp/css/gulpfile.js
@@ -13,4 +13,9 @@ function transformacaoCSS() {
     .pipe(gulp.dest("build/css"));
 }
 
-exports.default = series(transformacaoCSS);
+function copiarHTML () {
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('build'))
+}
+
+exports.default = series(transformacaoCSS, copiarHTML);
