5baa8d64f0ee6c489a02c501d48354a5f8f246d3
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-08 14:32:47 -0300

    webpack e CSS

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,6 +1,6 @@
 // Sistema de módulos do ECMASCRIPT
 import Pessoa from "./pessoa";
-// import './modulos/moduloA'
+import './assets/css/estilo.css'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,26 @@
-const webpack = require('webpack')
+const webpack = require("webpack");
+const MiniCssExtractPuglin = require("mini-css-extract-plugin")
 
 module.exports = {
     mode: "development",
     entry: "./src/principal.js",
     output: {
-        filename: 'principal.js',
-        path: __dirname + '/public'
-    }
-}
- 
+        filename: "principal.js",
+        path: __dirname + "/public",
+    },
+    plugins: [
+        new MiniCssExtractPuglin({
+            filename: "estilo.css"
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPuglin.loader,
+                //'style-loader', //adiciona a DOM injetando a tag <style>, é conflitante com o acima
+                'css-loader' // interpreta @import, url()
+            ]
+        }],
+    },
+};
