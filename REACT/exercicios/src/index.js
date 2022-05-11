1f310a069a8d9665705fd8764506842683a4375a
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-05-10 21:58:31 -0300

    módulo react

--- REACT/exercicios/src/index.js
+++ REACT/exercicios/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Primeiro from './componentes/primeiro'
 
 const elemento = <h1>React</h1>
 
-ReactDOM.render(elemento, document.getElementById('root'))
+ReactDOM.render(<h1>Primeiro</h1>, document.getElementById('root'))
